6615b94e47334a5cd406e7eb3af12808bdeeeec3
jacopoRufini <jacoporufini97@gmail.com>
2018-03-21 11:14:26 +0100

    interface updated

--- resources/views/Products/create.blade.php
+++ resources/views/Products/create.blade.php
@@ -1,8 +1,35 @@
-<form action="/products" method="POST">
-  {!! csrf_field() !!}
-<input type="text" name="name" placeholder="Nome">
-<input type="text" name="brand" placeholder="Brand">
-<input type="number" name="quantity" placeholder="Quantità">
-<input type="number" step="any" name="price" placeholder="Price">
-<input type="submit" value="Crea">
-</form>
+@extends('layouts.default')
+@section('title')
+	Aggiungi utente
+@endsection
+
+@section('body')
+<div class="row">
+	<form method="POST" action ="/users">
+		{!! csrf_field() !!}
+		<div class="col-lg-3 col-md-offset-4">
+			<div class="well well-lg">
+			    <div class="form-group">
+			        <label>Nome</label>
+			        <input type='text' class="form-control" name='name' placeholder="Es: Lucida labbra">
+			    </div>
+			    <div class="form-group">
+			        <label>Marca</label>
+			        <input type='text' class="form-control" name='brand' placeholder="Es: Sephora">
+			    </div>
+			    <div class="form-group">
+			        <label>Quantità</label>
+			        <input type='number' class="form-control" name='quantity' placeholder="Es: 12">
+			    </div>
+			    <div class="form-group">
+			        <label>Prezzo</label>
+			        <input type='number' class="form-control" name='price' placeholder="Es: 20,99" step="0.01">
+			    </div>
+				<input type="hidden" name="remember_token" value="{{ str_random(10) }}">
+				<p class="help-block">* Ogni campo è obbligatorio.</p>
+				<input type='submit' class="btn btn-outline btn-success" name='Create'>
+		</form>
+				</div>
+		</div>
+	</div>
+@endsection
