59c828cdf12a838a93c819be8ae479bba168f46a
jacopoRufini <jacoporufini97@gmail.com>
2018-03-24 11:24:54 +0100

    killed test files

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        
+        $this->middleware('auth');
     }
 
     /**
@@ -25,11 +25,5 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
-    }
-
-    public function test()
-    {
-        $sale = Sale::find(1);
-        return view('test',compact('sale'));
     }
 }
--- resources/views/test.blade.php
+++
@@ -1,5 +0,0 @@
-@foreach($sale->productSale as $productSale)
-	@foreach($productSale->product as $product)
-		{{ $product->name }}
-	@endforeach
-@endforeach
